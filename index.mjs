fc4d103ed5c199317b2107bf41d5ea37136dfdc6
Emilie Godin-Depaul <em.godin80@yahoo.com>
2023-02-20 11:05:02 +0000

    validating user input for title, added license list and fixed typos

--- index.mjs
+++ index.mjs
@@ -13,47 +13,64 @@ projectQuestions
 } = await inquirer.prompt([
     {
         type: "input",
-        message: "What is the name of your project?"
+        message: "What is the name of your project?",
         name: "projectTitle",
+        // checking that user has entered title:
+        validate: (projectTitle) => {
+            if (projectTitle) {
+              return true;
+            } else {
+              console.log("Please enter your project title");
+              return false;
+            }
+        },
     },
     {
         type: "input",
-        message: "Please describe your project:"
+        message: "Please describe your project:",
         name: "projectDescription",
     },
     {
         type: "input",
-        message: "Please explain how to install for first use:"
+        message: "Please explain how to install for first use:",
         name: "projectInstallation",
     },
     {
         type: "input",
-        message: "Please talk the user through the steps to use your project:"
+        message: "Please talk the user through the steps to use your project:",
         name: "projectUsage",
     },
     {
         type: "list",
-        message: "Please choose a type of license:"
+        message: "Please choose a type of license:",
         name: "projectLicense",
+        choices: [
+            "MIT License",
+            "Mozzilla Public License 2.0",
+            "Open Database License (ODbl)",
+            "The Unilicense",
+            "Boost Software License 1.0",
+            "Apache 2.0 License",
+          ],
     },
     {
         type: "input",
-        message: "Please list the people who supported your project or any contributing sources:"
+        message: "Please list the people who supported your project or any contributing sources:",
         name: "projectContributing",
     },
     {
         type: "input",
-        message: "Please enter instructions on how to test your project:"
+        message: "Please enter instructions on how to test your project:",
         name: "projectTests",
     },
     {
         type: "input",
-        message: "Please enter your GitHub URL:"
+        message: "Please enter your GitHub URL:",
         name: "githubQuestions",
     },
     {
     type: "input",
-    message: "Please enter your email address:"
+    message: "Please enter your email address:",
     name: "emailQuestions",
     },
 ])
